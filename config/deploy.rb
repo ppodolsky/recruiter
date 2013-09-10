247ef0a991a501a9b84215d337678350302d406a
Jeffrey Horn <jrhorn424@gmail.com>
2013-09-10 15:06:23 -0400

    Customize capfiles for generic deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,24 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "recruiter"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :repository,  "git@github.com:jrhorn424/recruiter.git"
+set :branch, "master"
+# set :git_enable_submodules, 1 # tell cap to use submodules if you're using them
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_via, :remote_cache # deploy by asking remote to checkout repo. requires key auth.
+set :ssh_options, { :forward_agent => true } # use local keys instead of the ones on the server.
+default_run_options[:pty] = true  # Must be set for the password prompt
+                                  # from git to work
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, "deploy"  # The server's user for deploys
+
+server "app.example.com", :app, :web, :db, :primary => true
+
+# role :web, "app.example.com"                          # Your HTTP server, Apache/etc
+# role :app, "app.example.com"                # This may be the same as your `Web` server
+# migrations unnecessary since we're using mongodb
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +27,10 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
