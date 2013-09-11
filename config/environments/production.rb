db3ec544660a10b3d1d0bb25124baffd05adaba8
Jeffrey Horn <jrhorn424@gmail.com>
2013-09-11 10:19:14 -0400

    Configure for memcache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Recruiter::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, '127.0.0.1:11211', {:namespace => "production"}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
