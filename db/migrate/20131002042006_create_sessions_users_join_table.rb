ca109fd15ed3723308cbb448851e7d62a23c3886
mlfreer <1991F@inbox.ru>
2013-10-03 16:36:03 -0400

    added participated and invited to join table

--- db/migrate/20131002042006_create_sessions_users_join_table.rb
+++ db/migrate/20131002042006_create_sessions_users_join_table.rb
@@ -1,7 +1,10 @@
 class CreateSessionsUsersJoinTable < ActiveRecord::Migration
   def change
-    create_table :experiments_experimenters, id: false do |t|
+    create_table :sessions_users, id: false do |t|
       t.integer :session_id
       t.integer :user_id
+      t.boolean :inveted, :default => 0
+      t.boolean :participated, :default => 0
+    end
   end
 end
