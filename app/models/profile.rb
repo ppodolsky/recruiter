b20541e7ad8d73fb8c281b2aa65c86651eb705e4
Jeffrey Horn <jrhorn424@gmail.com>
2013-10-04 12:43:15 -0400

    Initial profile model

---
+++ app/models/profile.rb
@@ -0,0 +1,19 @@
+class Profile < ActiveRecord::Base
+  belongs_to :user
+  validates :user, presence: true
+
+  validates :first_name, :last_name, :gender, :age, :ethnicity,
+    :years_resident, :class_year, :total_years, :year_started, :current_gpa,
+    :phone, presence: true
+
+  validates :secondary_email, format: { with: /\A[^@]+@[^@]+\z/, message: 'Not a valid e-mail address.' }
+  validates :secondary_email, uniqueness: true
+
+  normalize_attributes :secondary_email
+
+  [:first_name, :last_name, :profession, :ethnicity, :class_year].each do |attribute|
+    normalize_attribute attribute do |value|
+      value.is_a?(String) ? value.titleize.strip : value
+    end
+  end
+end
