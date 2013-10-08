10ef2927b57fc742b95da22997cb46126c22bc0e
Jeffrey Horn <jrhorn424@gmail.com>
2013-10-07 21:10:09 -0400

    Add ansible playbook for server configuration

---
+++ lib/deploy/ansible/roles/common/files/install-dotfiles.sh
@@ -0,0 +1,3 @@
+/home/deploy/.homesick/repos/homeshick/bin/homeshick clone jrhorn424/dotfiles --batch
+/home/deploy/.homesick/repos/homeshick/bin/homeshick clone jrhorn424/ruby --batch
+/home/deploy/.homesick/repos/homeshick/bin/homeshick link --force
