f2acea759b0fd3050a75d3c62aad504345b1d060
Jeffrey Horn <jrhorn424@gmail.com>
2013-11-11 18:05:06 -0500

    Remove duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Recruiter::Application.routes.draw do
+  # user accounts and profiles
   devise_for :users
-  resources :users do
+  scope :users do
     resource :profile
   end
 
+  # static content pages, provided by high_voltage
   root :to => 'pages#show', :id => 'welcome'
   get "/*id" => 'pages#show', :as => :page, :format => false
 end
