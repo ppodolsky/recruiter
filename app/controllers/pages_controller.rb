622e157c551ff21bd5e965d8762acafc86bbe2ed
Jeffrey Horn <jrhorn424@gmail.com>
2013-11-11 18:21:16 -0500

    Use inherited resources for semi-static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
-class PagesController < ApplicationController
+class PagesController < InheritedResources::Base
   include HighVoltage::StaticPage
+
+  defaults :resource_class => User, :collection_name => 'users', :instance_name => 'user'
 end
