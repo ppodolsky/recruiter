9a5a3706a44baa478f50cc3e0457441ee8773f6e
Jeffrey Horn <jrhorn424@gmail.com>
2013-11-18 18:08:34 -0500

    Remove migrations and initializer for rolify

--- db/migrate/20131003233531_create_categories_experiments_join_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategoriesExperimentsJoinTable < ActiveRecord::Migration
-    def change
-       create_table :categories_experiments, id: false do |t|
-          t.integer :experiment_id
-          t.integer :category_id
-        end
-    end
-  end
-  
