530308f60f5c223c0732c924e598907ee9759d3d
Jeffrey Horn <jrhorn424@gmail.com>
2014-01-16 10:10:02 -0500

    Remove ansible deployment scripts. These servers now handle more than just app configuration, so server configuration should be tracked in a separate project.

--- lib/deploy/ansible/roles/common/files/install-chruby.sh
+++
@@ -1,6 +0,0 @@
-wget -O chruby-0.3.6.tar.gz https://github.com/postmodern/chruby/archive/v0.3.6.tar.gz
-tar -xzvf chruby-0.3.6.tar.gz
-cd chruby-0.3.6/
-sudo make install
-cd ..
-rm -rf chruby-0.3.6.tar.gz chruby-0.3.6
--- lib/deploy/ansible/roles/common/files/install-dotfiles.sh
+++
@@ -1,3 +0,0 @@
-/home/deploy/.homesick/repos/homeshick/bin/homeshick clone jrhorn424/dotfiles --batch
-/home/deploy/.homesick/repos/homeshick/bin/homeshick clone jrhorn424/ruby --batch
-/home/deploy/.homesick/repos/homeshick/bin/homeshick link --force
