4ae301f0847abc894125ca34c3bdf7f367b92414
Jeffrey Horn <jeff@jrhorn.me>
2014-03-10 15:55:45 -0400

    Fix build error caused by seed-fu expecting RAILS_ENV to be set

--- config/initializers/seed_fu.rb
+++ config/initializers/seed_fu.rb
@@ -6,11 +6,11 @@ SeedFu.fixture_paths.reject! { |i| i.match(/fixtures/) }
 if ENV['RAILS_ENV'] == 'development'
   SeedFu.fixture_paths.unshift(
     File.expand_path(Rails.root.join('db','samples')),
-    File.expand_path(Rails.root.join('db','samples',ENV['RAILS_ENV']))
+    File.expand_path(Rails.root.join('db','samples',ENV['RAILS_ENV'] || ''))
   )
 end
 
 SeedFu.fixture_paths.unshift(
   File.expand_path(Rails.root.join('db','seeds')),
-  File.expand_path(Rails.root.join('db','seeds',ENV['RAILS_ENV']))
+  File.expand_path(Rails.root.join('db','seeds',ENV['RAILS_ENV'] || ''))
 )
