e79180bd71c955fbd2c064d7bfedc76a3fd39cfa
PashaPodolsky <ppodolsky@me.com>
2014-04-24 22:34:15 +0400

    redirect admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(user)
-    return dashboard_path if user.is_administrator?
     return experiments_path if user.is_experimenter?
     return edit_user_registration_path
   end
