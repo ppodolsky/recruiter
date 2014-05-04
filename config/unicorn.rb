9137494decdfecf8f9e7a0df7e492e08645b1ca5
PashaPodolsky <ppodolsky@me.com>
2014-05-04 13:33:55 +0400

    fixed unicorn 2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,54 +1,22 @@
-shared_path = "/u/apps/recruiter/shared"
-
-worker_processes 4
-timeout 60
-
-pid "#{shared_path}/pids/unicorn.pid"
-
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
-listen "/tmp/unicorn.sock", :backlog => 64
-listen 8080, :tcp_nopush => true
-
-stderr_path "#{shared_path}/log/unicorn.stderr.log"
-stdout_path "#{shared_path}/log/unicorn.stdout.log"
-
-# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and
-  GC.copy_on_write_friendly = true
-
-check_client_connection false
 
 before_fork do |server, worker|
-  old_pid = "#{shared_path}/pids/unicorn.pid.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
   defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-  sleep 1
+      ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  # the following is *required* for Rails + "preload_app true",
   defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+      ActiveRecord::Base.establish_connection
 end
