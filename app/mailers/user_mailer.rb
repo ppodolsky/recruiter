f1a5eabb3f7d6f11b9e0ec8e03ff13b6483c794b
Pasha Podolsky <ppodolsky@me.com>
2014-09-04 22:20:39 +0400

    fixed email with sessions list

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -40,7 +40,7 @@ class UserMailer < Devise::Mailer
     template.gsub!('@name', user.name)
     template.gsub!('@reward', "#{experiment.reward}$")
     template.gsub!('@timeline_url', timeline_url)
-    template.gsub!('@session_list', "\n" + experiment.sessions.map {|x| "* #{x.start_time_display}"}.join("\n") + "\n")
+    template.gsub!('@session_list', "\n" + Session.where(experiment_id: experiment.id, finished: false).where('registration_deadline > ?', Time.now).map {|x| "* #{x.start_time_display}"}.join("\n") + "\n")
     mail(:to => user.email,
          :content_type => 'text/html',
          :subject => 'ICES Experiment Invitation',
