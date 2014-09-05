157e1ac1021f114e04fc028fe5af19b9967405c4
Pasha Podolsky <ppodolsky@me.com>
2014-09-05 14:25:38 +0400

    fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :first_name, :last_name
 
 
+  validate :is_corporate_email?
   validates :secondary_email, format: {
       with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i,
       multiline: true,
