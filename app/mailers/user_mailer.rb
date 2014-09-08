a6e02d649b7de7e8cad96864d40e9f57068d7861
Pasha Podolsky <ppodolsky@me.com>
2014-09-09 01:08:48 +0400

    email fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 require 'kramdown'
 class UserMailer < Devise::Mailer
   @@host = 'ices-experiments.herokuapp.com'
-  default from: 'noreply@gmu.edu'
+  default from: 'ices-experiments@gmu.edu'
 
   def confirmation_instructions(user, token, opts={})
     send_from_db(user.email, Recruiter::Application.routes.url_helpers.user_confirmation_url(:confirmation_token => token, :host => @@host), 'confirm')
