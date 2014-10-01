8b35f04fe34bea69f167ee69ffea83b87e60bff7
Pasha Podolsky <ppodolsky@me.com>
2014-10-02 00:33:37 +0400

    fixed invitations

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,6 +1,6 @@
 class Experiment < ActiveRecord::Base
   before_save :default_values
-  has_many :sessions, :dependent => :delete_all, :through => :registrations
+  has_many :sessions, :dependent => :delete_all
   has_many :assignments, :dependent => :delete_all
   has_many :users, through: :assignments
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id', inverse_of: :experiments
