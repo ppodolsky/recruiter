071f459ea5ac690c2fdb5476567ffeed2e37746f
Pasha Podolsky <ppodolsky@me.com>
2014-10-03 10:28:07 +0400

    fixes for subject password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -15,16 +15,7 @@ class PasswordsController < Devise::PasswordsController
     redirect_to root_path
   end
   def update
-    self.resource = User.reset_password_by_token(resource_params)
-
-    if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-      set_flash_message(:notice, flash_message) if is_navigational_format?
-      sign_in(resource_name, resource)
-      respond_with resource, :location => after_resetting_password_path_for(resource)
-    else
-      respond_with resource
-    end
+    puts resource_params
+    super
   end
 end
