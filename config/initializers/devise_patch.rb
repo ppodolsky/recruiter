3cadf97a563c43626238db2f7b022bcb9ca9d1d2
Pasha Podolsky <ppodolsky@me.com>
2014-10-03 11:04:59 +0400

    fixes for subject password

--- config/initializers/devise_patch.rb
+++ config/initializers/devise_patch.rb
@@ -2,16 +2,13 @@ module Devise
   module Models
     module Recoverable
       def reset_password!(new_password, new_password_confirmation)
-        user = self.becomes(User.class)
-        user.password = new_password
-        user.password_confirmation = new_password_confirmation
+        self.password = new_password
+        self.password_confirmation = new_password_confirmation
 
-        if user.valid?
-          user.clear_reset_password_token
-          user.after_password_reset
-        end
+        clear_reset_password_token
+        after_password_reset
 
-        user.save
+        save(validate: false)
       end
     end
   end
