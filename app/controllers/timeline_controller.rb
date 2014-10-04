48f9d0747dbb77a0ff1d8c65edbdbd213bfa8103
Pasha Podolsky <ppodolsky@me.com>
2014-10-04 22:25:28 +0400

    fixes

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -7,6 +7,7 @@ class TimelineController < ApplicationController
     @incoming = current_user.sessions.where(finished: false).order('start_time ASC')
     @opened = Session
       .where(reservation: false)
+      .where(finished: false)
       .where(experiment_id: current_user.experiments)
       .where('registration_deadline > ?', Time.now)
       .where.not(id: current_user.sessions)
