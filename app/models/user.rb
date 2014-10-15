98a6d8a77dc9d00fec20e8d0ef2368b30c14500b
Pasha Podolsky <ppodolsky@me.com>
2014-10-15 23:20:13 +0400

    fixed stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -134,7 +134,7 @@ class User < ActiveRecord::Base
     end
   end
   def check_and_suspend!
-    previous = self.sessions.select('*').joins(:registrations).where(finished: true).where('start_time > ?', self.suspended_at).order(start_time: :desc)
+    previous = self.sessions.select('*').includes(:registrations).where(finished: true).where('start_time > ?', self.suspended_at).order(start_time: :desc)
     if self.registrations.finished.where(shown_up: false).where(session_id: self.sessions.where('start_time > ?', self.suspended_at).pluck(:id)).count > 3 or
         (previous.size >= 3 and previous[0,3].all? {|x| x.present? and not x.shown_up})
       self.suspend!
