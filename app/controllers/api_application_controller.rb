e8f00f426bb108afaabd2ac432274a481e627771
mlfreer <1991F@inbox.ru>
2016-07-08 17:43:23 +0300

    user update

--- app/controllers/api_application_controller.rb
+++ app/controllers/api_application_controller.rb
@@ -2,6 +2,18 @@ class ApiApplicationController < ActionController::Base
 	private
 	
 		def validate_authentication_token
-		
+			@user = User.find_by_email(params[:email])
+			if @user .nil?
+				render :json => '{"error": "invalid email and token combination"}'
+				return
+			end
+			if !@user .valid_authentication_token(params[:token])
+				render :json => '{"error": "invalid email and token combination"}'
+				return
+			end
+			if @user.suspended      
+				render :json => '{"error": "Your account has been suspended. Contact to administrator to deal with it."}'	
+				return
+			end
 		end
 end
